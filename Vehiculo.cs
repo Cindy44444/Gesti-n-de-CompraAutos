a563fe2c7bff089dd39c2ed23bf5623aeedcd9d7
MAYA CITLALLI MARTINEZ MORALES <112901@alumnouninter.mx>
2025-05-22 12:36:53 -0600

    vehiculo

--- Vehiculo.cs
+++ Vehiculo.cs
@@ -8,5 +8,22 @@ namespace Gestión_de_CompraAutos
 {
     internal class Vehiculo
     {
+        public Vehiculo() { }
+
+        public Vehiculo(int id, string marca, string modelo, int anio, string color)
+        {
+            this.id = id;
+            this.marca = marca;
+            this.modelo = modelo;
+            this.anio = anio;
+            this.color = color;
+        }
+
+        public int id {  get; set; }
+        public string  marca { get; set; }
+        public string modelo { get; set; }
+        public int anio { get; set; }
+        public string color { get; set; }
+
     }
 }
