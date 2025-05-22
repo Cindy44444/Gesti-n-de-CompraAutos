8453ae10a176a0eee5fca3ffd4855494fb98513d
MAYA CITLALLI MARTINEZ MORALES <112901@alumnouninter.mx>
2025-05-22 13:08:25 -0600

    las acciones

--- acciones.cs
+++ acciones.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 
 namespace Gestión_de_CompraAutos
 {
-    internal class acciones
+    internal class acciones : IACCIONES
     {
+        public bool Actualizar(int id, string marca, string modelo, int anio, string color, double precio, string estado)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Agregar(int id, string marca, string modelo, int anio, string color, double precio, string estado)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Eliminar(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ExportarExcel()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ImportarExcel()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<auto> MostrarAuto()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
